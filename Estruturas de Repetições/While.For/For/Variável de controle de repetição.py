01423688066e8de6b9b540a2a6bf9ecbd268b3f5
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-08-22 21:41:02 -0300

    FOR. Variável de controle de repetição.py

---
+++ Estruturas de Repetições/While.For/For/Variável de controle de repetição.py
@@ -0,0 +1,17 @@
+#variável de controle de repetição:
+inicio=int(input('Digite o início: '))
+fim=int(input('Difite o fim: '))
+passo=int(input('Digite o passo: '))
+for c in range(inicio,fim+1,passo):
+    print(c)
+print('FIM')
+
+Results:
+igite o início: 2
+Difite o fim: 9
+Digite o passo: 3
+2
+5
+8
+FIM
+==============================================================================
