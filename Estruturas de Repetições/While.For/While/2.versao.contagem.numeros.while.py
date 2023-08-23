8b4921d9f9ff1b688c98ef68367ded5eff204e72
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-08-23 12:40:28 -0300

    Update 2.versao.contagem.numeros.while.py

--- Estruturas de Repetições/While.For/While/2.versao.contagem.numeros.while.py
+++ Estruturas de Repetições/While.For/While/2.versao.contagem.numeros.while.py
@@ -1,17 +1,20 @@
-
-
+#Contagem de números
 while True:
-    n = str(input("para saber todos os números impares e pares entre 1 e 50 digite: \n (A) impar \n (B) par \n")).upper()
+    n = str(input('''
+Para saber todos os números impares e pares entre 1 e 50 digite:
+        (A) impar
+        (B) par ''')).upper()
     if "B" in n:
         for n in range(2, 51, 2):
             print(n, end='.')
-        print("\nEsses são os números pares entre 1 e 50")
+        print("\nEsses são os números pares entre 1 e 50.")
         break
     if "A" in n:
         for n in range(1, 51, 2):
             print(n, end='.')
-        print("\nEsses são os números ímpares entre 1 e 50")
+        print("\nEsses são os números ímpares entre 1 e 50.")
         break
     if n != "A" or "B":
-        print("Somente a opção A ou B são permitidas")
-print("Espero que tenha sido uma experiência agradável")
+        print("\nSomente a opção A ou B são permitidas...")
+print("Fim")
+
