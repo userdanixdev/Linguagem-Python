73e7913fd070d9758f82c11f3fc24511027ac646
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-08-28 17:05:29 -0300

    Update and rename while to while.py

---
+++ Estruturas de Repetições/While.For/While/while.py
@@ -0,0 +1,31 @@
+A estrutura de repetição 'WHILE' produz laços indeterminados.
+Estrutura básica:
+c=1
+while c<10:
+  print(c)
+  c = c+ 1
+print('Fim')
+===================================
+
+Sem limites de valores. Por exemplo:
+pela estrutura de repetição por variável de controle for in range(), devemos dizer
+de onde vai até onde vai.
+O WHILE não.
+Exemplo:
+n=1 
+while n !n = 0:
+  n = int(input('Digite um valor: '))
+print('Fim')
+Os resultados não terão FIM!!
+===============================================
+Exemplo02:
+r = 'S'
+while r == 'S':
+  n = int(input('Digite um valor: '))
+  r = str(input('Quer continuar? [S/N] ')).upper()
+print('FIM')  
+================================================
+
+
+
+
