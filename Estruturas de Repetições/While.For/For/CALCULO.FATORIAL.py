f811bbaab5895eeab22894a25d1784eb514901f6
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-08-29 12:37:54 -0300

    Update CALCULO.FATORIAL.py

--- Estruturas de Repetições/While.For/For/CALCULO.FATORIAL.py
+++ Estruturas de Repetições/While.For/For/CALCULO.FATORIAL.py
@@ -8,3 +8,12 @@ for i in range(1,numero+1):
     fatorial = fatorial *i
     print(f'O resultado de {numero} é {fatorial}.')
 
+Results:
+
+Digite um número para saber o fatorial: 5
+O resultado de 5 é 1.
+O resultado de 5 é 2.
+O resultado de 5 é 6.
+O resultado de 5 é 24.
+O resultado de 5 é 120.
+
