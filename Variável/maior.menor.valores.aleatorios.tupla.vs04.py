2451f57ffb3c2cac55389887c5c72f0278fd8dd4
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-09-28 17:18:35 -0300

    Add files via upload

---
+++ Variável/maior.menor.valores.aleatorios.tupla.vs04.py
@@ -0,0 +1,15 @@
+#Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
+#Depois disso, mostre a listagem de números gerados
+#Também indique o menor e o maior valor que estão na tupla.
+
+#Versão 04
+
+import random
+maior = menor = int
+tupla=(random.randint(0,10),random.randint(0,10),random.randint(0,10),random.randint(0,10),random.randint(0,10))
+for i in range(0,5):
+       if(i== 0 or tupla[i]>maior):
+           maior=tupla[i]
+       if(i==0 or tupla[i]<menor):
+           menor=tupla[i]
+print(f'{tupla},\n Maior número:{maior},\n O menor é:{menor}.')       
