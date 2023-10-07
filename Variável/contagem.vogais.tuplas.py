ecc4c4750081b37ab14fc7c642a1ddb75f74f371
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-10-07 20:13:20 -0300

    Update contagem.vogais.tuplas.py

--- Variável/contagem.vogais.tuplas.py
+++ Variável/contagem.vogais.tuplas.py
@@ -9,3 +9,17 @@ for p in palavras:
     for letra in p:
         if letra.lower() in 'aeiou':
             print(letra,end=' ')
+
+Resultado:
+Na palavra APRENDER temos a e e 
+Na palavra PROGRAMAR temos o a a 
+Na palavra LINGUAGEM temos i u a e 
+Na palavra PYTHON temos o 
+Na palavra CURSO temos u o 
+Na palavra GRATIS temos a i 
+Na palavra ESTUDAR temos e u a 
+Na palavra PRATICAR temos a i a 
+Na palavra TRABALHAR temos a a a 
+Na palavra MERCADO temos e a o 
+Na palavra PROGRAMADOR temos o a a o 
+Na palavra FUTURO temos u u o 
