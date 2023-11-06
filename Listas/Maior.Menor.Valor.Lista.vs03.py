7c13b19dd90cd58a1c48bb65a42624e6d26c5b73
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-11-06 19:08:18 -0300

    Best Version
    Melhor versão na identificação da posição do melhor e menor número de uma lista vazia.

--- Listas/Maior.Menor.Valor.Lista.vs03.py
+++ Listas/Maior.Menor.Valor.Lista.vs03.py
@@ -12,6 +12,18 @@ print(f'Os números digitados foram: {valores}')
 print(f'O maior número foi {max(valores)} na(s) posição(ões): {valores.index(max(valores))}°') 
 print(f'O menor número digitado é: {min(valores)} na posição {valores.index(min(valores))}°')
 
+Results:
+
+Digite número: 15
+Digite número: 25
+Digite número: 20
+Digite número: 20
+Digite número: 21
+Os números digitados foram: [15, 25, 20, 20, 21]
+O maior número foi 25 na(s) posição(ões): 1°
+O menor número digitado é: 15 na posição 0°
+
+================================================================
 
 
 
