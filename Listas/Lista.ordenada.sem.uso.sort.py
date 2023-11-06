702c2083cb481c20209db1632479c72827ca4a16
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-11-06 18:54:10 -0300

    1°versão excelente
    com print de 'adicionado na lista'

--- Listas/Lista.ordenada.sem.uso.sort.py
+++ Listas/Lista.ordenada.sem.uso.sort.py
@@ -19,7 +19,21 @@ for c in range(0,5):
              print(f'Adicionado na posição {pos} da lista...')
             break
             pos = pos + 1
-print(f'Os valores digitados em ordem foram: {lista}')            
+print(f'Os valores digitados em ordem foram: {lista}')      
+
+Results:
+
+Digite um valor: 15
+Digite um valor: 20
+Adicionado ao final da lista...
+Digite um valor: 13
+Adicionado na posição 0 da lista...
+Digite um valor: 55
+Adicionado ao final da lista...
+Digite um valor: 36
+Os valores digitados em ordem foram: [13, 15, 20, 55]
+
+=======================================================
 
 
 
