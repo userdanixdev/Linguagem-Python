d6db0b0e4279e6ff6eb1c597408b679849516604
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-11-06 19:16:58 -0300

    Excelente versão!
    A versão nos mostra a soma da lista ao colocar cada valor de entrada, um por um.

--- Listas/Valores.unicos.lista.vs02.py
+++ Listas/Valores.unicos.lista.vs02.py
@@ -8,7 +8,7 @@ lista=[]
 while True:
     n1=int(input('Digite um número: '))
     if n1 in lista:
-        print('Digite um valor único!')
+        print('Digite um valor que não está na lista!')
     else:
         lista.append(n1)
     n2=input('Deseja continuar? [S/N]')
@@ -17,7 +17,27 @@ while True:
     lis=sorted(lista)
     print(f'Você digitou os valores{lis}.')
 print(f'Você digitou os valores{lis}.')
-    
+print('Fim do programa')
+
+Resultado:
+
+Digite um número: 5
+Deseja continuar? [S/N]s
+Você digitou os valores[5].
+Digite um número: 10
+Deseja continuar? [S/N]s
+Você digitou os valores[5, 10].
+Digite um número: 15
+Deseja continuar? [S/N]s
+Você digitou os valores[5, 10, 15].
+Digite um número: 15
+Digite um valor único!
+Deseja continuar? [S/N]s
+Você digitou os valores[5, 10, 15].
+Digite um número: 1
+Deseja continuar? [S/N]n
+Você digitou os valores[5, 10, 15].
+===================================================
 
 
 
