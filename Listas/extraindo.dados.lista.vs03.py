b7b6f4e349d4df461b932389465b8b6e2860bde0
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-11-05 22:02:19 -0300

    Version 3

---
+++ Listas/extraindo.dados.lista.vs03.py
@@ -0,0 +1,26 @@
+
+# EXTRAINDO DADOS DA UMA LISTA
+
+#Crie um programa que vai ler vários números e colocar em uma lista.
+#Depois disso, mostre:
+ #   A) Quantos números foram digitados.
+ #   B) A lista de valores, ordenada de forma decrescente.
+ #   C) Se o valor 5 foi digitado e está ou não na lista.
+
+ # Versão 03 #
+
+contador = 0
+resposta = 's'
+lista=[]
+while resposta.lower()=='s':
+    lista.append(int(input('Digite um valor: ')))
+    resposta = input('Quer continuar? [S/N]').lower()
+    contador =  contador + 1
+lista.sort(reverse=True)
+print(f'Vc digitou {contador} elementos.\n E os valores em ordem decrescente foram: {lista}).')
+if 5 in lista:
+    print('O valor 5 está na lista.')
+else:
+    print('O valor 5 não foi encontrado.')
+                 
+                 
