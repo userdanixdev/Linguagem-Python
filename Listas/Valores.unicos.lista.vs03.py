5a0faaa64448f71bcd9ec3448851d9639f2f8b1b
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-11-16 17:56:43 -0300

    Update Valores.unicos.lista.vs03.py

--- Listas/Valores.unicos.lista.vs03.py
+++ Listas/Valores.unicos.lista.vs03.py
@@ -15,11 +15,28 @@ while True:
         print('Você já adicionou esse número...')
     resposta=input('Deseja continuar? [S/N]')
     while resposta not in 'SsNn':
-        resposta = input('Quer continuar?[S/N]')
+        resposta = input('Quer continuar?[S/N]')  # Irá fazer looping infinito até digitar ou 's','S','N','n'
     if resposta in 'nN':
         break
 nova_lista=sorted(lista)
 print(f'Os números que você digitou foram: {nova_lista} em ordem crescente.')
+
+Resultado no console:
+
+Digite um número inteiro: 456
+Numero adicionado com sucesso.
+Deseja continuar?[S/N]r
+Quer continuar?[S/N]r
+Quer continuar?[S/N]s
+Digite um número inteiro: 123
+Numero adicionado com sucesso.
+Deseja continuar?[S/N]s
+Digite um número inteiro: 689
+Numero adicionado com sucesso.
+Deseja continuar?[S/N]p
+Quer continuar?[S/N]n
+Os números que vc digitou foram [123, 456, 689].
+=============================================================================
 
 
 
