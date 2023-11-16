db992da92613debe3cb815200660d39ceefabd37
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-11-16 17:50:32 -0300

    Update extraindo.dados.lista.vs04.py

--- Listas/extraindo.dados.lista.vs04.py
+++ Listas/extraindo.dados.lista.vs04.py
@@ -12,9 +12,9 @@
 lista=list()
 while True:
     lista.append(int(input('Digite um valor: ')))
-    resp = input('Quer continuar?[S/N]').strip().upper()[0]
+    resp = input('Quer continuar?[S/N]').strip().upper()[0]   # Irá fazer com que o usuário digite a letra correta infinitamente
     while resp not in 'SsNn':
-        resp = input('Quer continuar?[S/N]').strip().upper()[0]
+        resp = input('Quer continuar?[S/N]').strip().upper()[0]  # Irá fazer com que o usuário digite a letra correta infinitamente
     if resp in 'nN':
         break
 print(f'Você digitou {len(lista)} elementos.')
@@ -24,3 +24,21 @@ if 5 in lista:
     print(f'O valor 5 faz parte da lista.')
 else:
     print('O valor 5 não faz parte desta lista.')
+
+Resultado:
+
+Digite um valor: 5
+Quer continuar?[S/N]r
+Quer continuar? [S/N].r
+Quer continuar? [S/N].s
+Digite um valor: 65
+Quer continuar?[S/N]t
+Quer continuar? [S/N].s
+Digite um valor: 1
+Quer continuar?[S/N]s
+Digite um valor: 32
+Quer continuar?[S/N]n
+Vc digitou 4 elementos.
+Os valores em ordem decrescente são [65, 32, 5, 1].
+O valor 5 faz parte da lista.
+
