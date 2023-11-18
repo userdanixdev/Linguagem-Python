c17d038eb2251e175e93909f744aa0c2c68ddaef
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-11-18 20:04:18 -0300

    Listas separadas
    Segue no cabeçalho do código o enunciado do que se pede.

---
+++ Listas/lista.pares.impares.py
@@ -0,0 +1,23 @@
+#Crie um programa onde o usuário possa digitar sete valores numéricos e
+#cadastre-os em uma lista única que mantenha separados os valores pares
+#e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+
+num=[[],[]]   # Declaro uma lista dentro de duas listas internas, a 1° é par.
+valor = 0
+num[0].sort()
+num[1].sort()
+for c in range(1,8):
+    valor = int(input(f'Digite o {c}° valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print('+'*50)
+print(f'Todos os valores: {num}.')
+print('='*50)
+print(f'Os valores ímpares digitados foram:{num[1]}\n e os pares:{num[0]}.')
+print('*'*40)
+
+
+
+
