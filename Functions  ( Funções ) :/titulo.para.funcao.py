6dfce4d544203afcd1de42e5ec2c7cfa8c69ccca
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2023-12-13 15:27:07 -0300

    Títulos na função - 'def'
    Comandos básicos para títulos com 'def'

---
+++ Functions  ( Funções ) :/titulo.para.funcao.py
@@ -0,0 +1,20 @@
+#Faça um programa que tenha uma função chamada escreva(), que receba um texto
+#qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
+
+#Títulos como função:
+
+def escreva(titulo):
+    tam=len(titulo) + 4 #<--- len é o tamanho da mensagem. 'O +4 para ficar uma borda.'
+    print('+'*tam)
+    print(f'  {titulo}')   ## Dois espaços na função para centralizar o título.
+    print('+'*tam)
+
+escreva('Olá, mundo.')
+escreva('Curso de Python no Youtube.')
+## AS LINHAS IRÃO ACOMPANHAR O TÍTULO ##
+escreva('Chega disso!')
+    
+
+    
+
+
