7d3ef93bf750d9b6ef89fcfdf84f49709efe73ca
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-04 19:21:36 -0300

    Create Objetos.py

---
+++ Classes/Objetos.py
@@ -0,0 +1,21 @@
+lst_num = ['data','Science','Academy','Nota',10,20]
+# a lst_num é um objeto, uma instância da classe lista em Python
+
+# Usamos a função type, para verificar o tipo de um objeto
+print(type(10))
+print(type([]))
+print(type(()))
+print(type({}))
+print(type('a'))
+
+Resultado:
+
+<class 'int'>
+<class 'list'>
+<class 'tuple'>
+<class 'dict'>
+<class 'str'>
+--------------//==========================//----------------------//---------
+
+
+
