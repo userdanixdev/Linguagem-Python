95fd40f428cae05572c518193fb7d1b634cf8d2d
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-04 22:19:04 -0300

    Create Exercicio.py

---
+++ Classes/Exercicio.py
@@ -0,0 +1,15 @@
+# Crie uma classe chamada Pessoa() com os atributos: nome, cidade, telefone e e-mail. Use pelo menos 2
+# métodos especiais na sua classe. Crie um objeto da sua classe e faça uma chamada a pelo menos um dos seus métodos
+# especiais
+
+class Pessoa():
+  def __init__(self,nome,cidade,telefone,email):
+    self.nome = nome
+    self.cidade = cidade
+    self.telefone = telefone
+    self.email = email
+    print('Objeto criado')
+  def __str__(self):
+    return 'O usuário'+self.nome+'mora na cidade'+self.cidade
+p1=pessoa('pele','Tres coracoes',99887766,'pele@gmail.com')
+srt(p1)
