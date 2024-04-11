4048d954da91d72f1b1bb3918dcc2d6655b19d8d
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-11 14:24:55 -0300

    Update Data_Frame.Python_Pandas.py

--- Pandas/Data_Frame.Python_Pandas.py
+++ Pandas/Data_Frame.Python_Pandas.py
@@ -23,32 +23,7 @@ data.shape
 # ( Primeiro: Nº de linhas, Segundo: COLUNAS)
 print(f'O DataFrame do arquivo lido em CSV possui {data.shape[0]} linhas/observações/registros \n e \n{data.shape[1]} colunas/atributos/variáveis')
                 
-                    ### CRIANDO UM DATAFRAME ###
-
-jogadores_df = pd.DataFrame({
-    'Nome':['Ronaldo','Ronaldinho','Kaká','Adriano','Juninho Pernambucano'],
-     'Idade':[30,28,28,26,31],
-      'Peso': [98.5,75,78,85,72],
-      'Posição':['Atacante','Meio-campo','Meio campo','Atacante','Meio campo']})                    
-                    
-jogadores_df.info()
-# informações do dataframe criado
-jogadores_df.columns
-## Esse método mostra somente as colunas do data frame
-list(jogadores_df.columns)                    
-## Irá mostrar as colunas em lista, por exemplo.
-jogadores_df_renomeado = jogadores_df.rename(columns={
-     'Nome': 'Nome Apelidado',
-      'Idade':'Idades' })
-jogadores_df_renomeado.info()
-print(jogadores_df_renomeado           )
-#################################################
-jogadores_df.rename(columns={'Nome': 'Nome Apelidado',
-      'Idade':'Idades' }, inplace=True)
-jogadores_df #<- Sendo assim, outra forma de renomear a coluna é com o 'inplace'
-#####################################################################
-# OUTRA FORMA DE RENOMEAR COLUNAS DE UMA VEZ:
-jogadores_df.columns = ['NOME', 'IDADE', 'PESO','POSIÇÃO']    
-jogadores_df
+                  
+    
 #####################################################################
 OK
