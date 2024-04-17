b685a75e2141cb83dfcad1685f570d9a7411f41f
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-17 09:39:39 -0300

    Create Introdução a Biblioteca.py

---
+++ Matplotlib/Introdução a Biblioteca.py
@@ -0,0 +1,7 @@
+!pip install matplotlib
+import matplotlib.pyplot as plt
+%matplotlib inline  <<- Comando Jupyter Notebook
+plt.plot([1,3,5],[2,4,7])
+plt.show()
+-------//------------//----------------//----------------//
+
