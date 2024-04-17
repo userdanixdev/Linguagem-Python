6753f032b7537252f39bdc9bf7cd22cb37adbc14
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-17 15:57:34 -0300

    Create Gráfico de Pizza.py

---
+++ Matplotlib/Gráfico de Pizza.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as plt
+
+fatias = [7,2,2,13]
+atividades = ['dormir','comer','passear','trabalhar']
+cores = ['olive','lime','violet','royalblue']
+plt.pie(fatias,
+        labels = atividades,
+        colors=cores,
+        startangle=90,
+        shadow = True,   #<- Sombra no gráfico
+        explode = (0,0,0.4,0))
