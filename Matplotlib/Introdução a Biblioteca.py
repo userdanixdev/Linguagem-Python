7f68689c95546fae094f17ced5f63022cfe68a24
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-17 09:45:37 -0300

    Update Introdução a Biblioteca.py

--- Matplotlib/Introdução a Biblioteca.py
+++ Matplotlib/Introdução a Biblioteca.py
@@ -5,3 +5,14 @@ plt.plot([1,3,5],[2,4,7])
 plt.show()
 -------//------------//----------------//----------------//
 
+#Outro exemplo:
+# Criar as variáveis - Massa de dados
+x=[2,3,5]
+y=[3,5,7]
+# Ponha os títulos PRINCIPAL, EIXO X E Y:
+plt.plot(x,y)
+plt.xlabel('Variável 1')
+plt.ylabel('Variável 2')
+plt.title('Teste Plot')
+-----------------//--------------------//------------------//----------
+
