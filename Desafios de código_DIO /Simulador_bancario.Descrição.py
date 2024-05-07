4030cb24bf55dcbcbd8b33e90152017b4ca0eec4
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-07 01:08:03 -0300

    Create Simulador_bancario.Descrição.py

---
+++ Desafios de código_DIO /Simulador_bancario.Descrição.py
@@ -0,0 +1,23 @@
+# Descrição do problema :
+  # Objetivo Geral:
+    # Criar um sistema bancário com as operações: sacar,depositar e visualizar de extrato.
+# Fomos contratados por um grande banco para desenvolver o seu novo sistema. Esse banco deseja modernizar suas operações e para isso 
+# escolheu a linguage Python. Para a primeira versão do sistema devemos implementar apenas 3 operações: depósito,saque,extrato.
+
+# Operações de depósito:
+# Apenas valores positivos
+# Trabalhar apenas com 1 usuário - Inicialmente não iremos identificar número de agência e conta.
+# Todos os depósitos devem ser armazenados em uma variável e exibidos na operação do extrato.
+
+# Operação de saque:
+  # O sistema deve permitir realizar 3 saques diários.
+  # Limite de saque de R$ 300,00 por saque.
+  # Caso não tenha saldo em conta, o sistema deve exibir uma mensagem informando a invalidez do saque.
+  # Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato.
+
+# Operação de extrato:
+  # Deve listar todos os depósitos e saques realizados na conta. 
+  # Além disso, exibir o saldo atual da conta.
+  # O formato é com duas casas decimais depois da vírgula ( R$ 000,00 )
+
+
