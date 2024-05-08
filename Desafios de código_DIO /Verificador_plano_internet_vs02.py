9c9776585d790fc719ff9fddebff4b6b3dd43393
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-08 18:20:52 -0300

    Update Verificador_plano_internet_vs02.py

--- Desafios de código_DIO /Verificador_plano_internet_vs02.py
+++ Desafios de código_DIO /Verificador_plano_internet_vs02.py
@@ -1,3 +1,16 @@
+# Desafio:
+# Uma empresa de telecomunicações deseja criar uma solução algorítmica que ajude aos seus clientes a escolherem o plano de internet ideal 
+# com base em seu consumo mensal de dados.
+# Para a resolução, você pode solicitar ao usuário que insira o seu consumo, sendo este um valor 'float'.
+# Crie uma função chamada 'recomendar_plano' para receber o consumo médio mensal de dados informado pelo cliente,
+# além de utilizar estruturas condicionais para fazer a verificação e retornar o plano adequado.
+
+'''Planos Oferecidos:
+
+- Plano Essencial Fibra - 50Mbps: Recomendado para um consumo médio de até 10 GB.
+- Plano Prata Fibra - 100Mbps: Recomendado para um consumo médio acima de 10 GB até 20 GB.
+- Plano Premium Fibra - 300Mbps: Recomendado para um consumo médio acima de 20 GB.'''
+
 def recomendar_plano(consumo_medio,plano_essencial,plano_prata,plano_premium):
   if consumo_medio <= plano_essencial:
     print( 'Plano Essencial Fibra - 50Mbps')
