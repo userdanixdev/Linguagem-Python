32a63698eb79970634ba44624bff3191c59ec4a4
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-11 16:35:32 -0300

    Update Cadastro.jogador.futebol.vs05.py

--- Dicionários/Cadastro.jogador.futebol.vs05.py
+++ Dicionários/Cadastro.jogador.futebol.vs05.py
@@ -7,21 +7,28 @@
 
 #Versão 05:
 
-jogador={}
-gol=[]
-totgols=0
-jogador['Nome']=input('Digite o nome do jogador: ')
-jogador['Partidas']=int(input(f'Digite quantas partidas o jogador {jogador["Nome"]} jogou: '))
-for count in range(1,jogador['Partidas']+1):
-    g=int(input(f'Quantos gols na {count}° partida o jogador {jogador["Nome"]} marcou: '))
-    totgols += g
-    gol.append(g)
-jogador['Gols']=gol
-jogador['Total de Gols']=totgols
-print('='*30)
+from time import sleep
+print(f'{'='*30}\n{'RENDIMENTO DO JOGADOR':^30}\n{'='*30}')
+jogador={}  # Variável jogador recebe um dicionário vazio
+gol = []    # Variável gol recebe uma lista vazia
+total_gols  = 0 # Variavel total_gols recebe contador numerico inteiro
+jogador['Nome']=input('Digite o nome do jogador: ') # Dicionário jogador recebe chave 'nome' com o valor a ser recebido pelo usuário
+jogador['Partidas']=int(input(f'Quantas partidas o {jogador["Nome"]} jogou: '))
+for count in range(1,jogador['Partidas']+1): # Loop de um intervalo de 1 a quantidade de partidas que o usuário colocou no dicionario
+        
+        g=int(input(f'Quantos gols na {count}º partida o jogador {jogador["Nome"]} marcou: ')) # Entrada de gols por partida para variavel 'g'
+        sleep(0.7)
+        total_gols += g # total_gols recebe o número de gols da variável 'g'
+        gol.append(g)   #  Lista gol recebe o número de gols da variável 'g' colocada pelo usuário
+jogador['Gols']=gol # Dicionário jogador recebe o valor da variável 'gol' que são os números de gols para a chave 'Gols'
+jogador['Total de Gols']=total_gols # Dicionario jogador recebe o valor da variável 'total_gols' para a chave 'Total de Gols'
+# dados gerais do jogador:
+sleep(0.5)
 print(f'O jogador {jogador["Nome"]} jogou {jogador["Partidas"]} partidas.')
-for p in range(1,jogador['Partidas'] +1):
-    print(f' => Na {p}° partida ele marcou {jogador["Gols"][p-1]} gols.')
-print(f'Totalizando em {totgols} gols.')
-print('+'*30)
-                       
+for p in range(1,jogador['Partidas']+1):  # 'p' é o numero de partidas dentro do dicionario 'Partidas'
+    sleep(0.5)
+    print(f'Na {p}º partida ele marcou {jogador["Gols"][p-1]} gols.')
+sleep(0.5)
+print(f'Totalizando em {total_gols} gols.')    
+        
+
