4711a2647d2ce3b0fbf1d0f3e19e53ec60f9aebf
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-13 17:25:19 -0300

    Update Média_Indiviual_2.0_.py

--- Dicionários/Média_Indiviual_2.0_.py
+++ Dicionários/Média_Indiviual_2.0_.py
@@ -5,6 +5,7 @@
 
 # Obs : A sintaxe para {chave:valor} sem fechar o parênteses irá fazer com que outros declarações de dicionario seja escritas por cima.
 # Ao listar um dicionário, a função items poderá percorrer as listas criadas dentro do dicionário
+# No entanto, não há necessidade de criar uma lista vazia
 aluno={'Nome':str(input('Nome: '))} # Variável aluno recebe chave nomeada 'nome' e entrada do usuário dentro do dicionário
 aluno['Média']=float(input(f'Digite a média de {aluno["Nome"]}: '))
 aluno['Situação']='aprovado'if aluno['Média']>=7 else 'Reprovado' if aluno['Média']<6 else 'Recuperação'
