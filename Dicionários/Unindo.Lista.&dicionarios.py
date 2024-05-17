880969b1049ac4d5476e40a044f39bb52998a45c
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-17 19:15:17 -0300

    Update Unindo.Lista.&dicionarios.py

--- Dicionários/Unindo.Lista.&dicionarios.py
+++ Dicionários/Unindo.Lista.&dicionarios.py
@@ -1,12 +1,12 @@
-#Unindo dicionários e listas
+# Unindo dicionários e listas
 
-#Crie um programa que leia nome, sexo e idade de várias pessoas,
-#guardando os dados de cada pessoa em um dicionário e todos os dicionários
-#em uma lista. No final, mostre:
-#A) Quantas pessoas foram cadastradas
-#B) A média de idade
-#C) Uma lista com as mulheres
-#D) Uma lista de pessoas com idade acima da média
+# Crie um programa que leia nome, sexo e idade de várias pessoas,
+# guardando os dados de cada pessoa em um dicionário e todos os dicionários
+# em uma lista. No final, mostre:
+# A) Quantas pessoas foram cadastradas
+# B) A média de idade
+# C) Uma lista com as mulheres
+# D) Uma lista de pessoas com idade acima da média
 
 pessoa=dict()
 galera=list()
