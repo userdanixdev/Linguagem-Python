b5672b6e03e51546de33533aed39a8573647bd9d
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-22 18:51:49 -0300

    Update Unindo.Lista&dicionarios_vs005.py

--- Dicionários/Unindo.Lista&dicionarios_vs005.py
+++ Dicionários/Unindo.Lista&dicionarios_vs005.py
@@ -38,6 +38,51 @@ if usuarios_acima_da_media:
   print('Os usuários acima da média são:', ', '.join(usuarios_acima_da_media))
 else:
   print('Nenhum usuário acima da média.')
-  
+
+dados=dict()
+# Essa é a lista a qual acessaremos todos os dicionários
+users=list()
+# Coleta de dados para lista/dict:
+soma = 0
+media = 0
+while True:
+  dados['Nome']=input('Nome do usuário: ')         # Dicionário recebe chave nome e imput com um valor 
+  dados['idade']=int(input('Idade do usuário: '))  # Dicionário dados recebe chave 'idade' e input a ser inserido
+  soma += dados['idade']
+  dados['sexo']=input('Sexo do usuário [M/F]: ').upper().strip()
+  while True:
+    if dados['sexo'] in 'MF':
+      print('Usuário cadastrado com sucesso.')
+      break
+    else:      
+      print('ERROR')
+      dados['sexo']=input('Digite um sexo válido [M/F]: ').upper().strip()
+  users.append(dados.copy())   
+  print('='*10)
+  while True:
+    resp=input('Desejas Continuar [S/N]?').upper().strip()
+    if resp in 'SN':
+      break
+      print('ERROR')
+  if resp == 'N':
+    break
+
+# Expor os dados: 
+print(f' Foram cadastradas {len(users)} pessoas.')
+media=soma/len(users)
+print(f' A média de idade é de {media:.2f} anos.')
+print(f' As mulheres cadastradas foram: ', end='')
+for p in users:     # Looping em que para cada chave 'p' em usuários imprime o valor do dicionário 'nome'
+  if p['sexo']=='F':
+    print(f'[{p["Nome"]}]',end='')
+print()
+print(f' Os usuários acima da média são: ',end='')
+for p in users:  
+  if p['idade']>media:
+    print(f'[{p["nome"]}]', end='')
+
+print()
+print('Programa finalizado')
+
 
 
