dc752ae14dadc95c4716645fe6bf2a08388ec515
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-05 18:09:09 -0300

    Create pipenv.py

---
+++ Ambientes Virtuais/pipenv.py
@@ -0,0 +1,3 @@
+## Pipenv é uma ferramenta de gerenciamento de pacotes que combina a gestão de dependências com a criação de ambiente virtual para seus projetos e adiciona/remove
+## pacotes automaticamente do arquivo Pipfile conforme você instala e desinstala pacotes.
+
