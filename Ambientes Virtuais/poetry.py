9f7247066a6633ee4a02be40d7ea4d2bebe259a7
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-05 20:03:30 -0300

    Update poetry.py

--- Ambientes Virtuais/poetry.py
+++ Ambientes Virtuais/poetry.py
@@ -1,1 +1,12 @@
-poetry
+## Poetry é outra ferramenta de gerenciamento de dependências para Python, concorrente do pipenv que permite declarar bibliotecas do seu projeto dentro de um ambiente virtual
+# que permite gerenciar para você.
+# Tambem pode disponibilizar pacotes dentro do PyPI.
+
+## Para instalar:
+pip install poetry  # Para instalar
+poetry new myproject # Para criar um novo projeto
+cd myproject # Entrar no projeto em ambiente virtual
+poetry add numpy  # Adicionar dependência
+poetry remove numpy # Remover dependência
+
+
