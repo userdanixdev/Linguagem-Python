14c70dad74aff224bbc7a8a2a9b1ab406318e8c6
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-05 18:35:40 -0300

    Update pipenv.py

--- Ambientes Virtuais/pipenv.py
+++ Ambientes Virtuais/pipenv.py
@@ -1,3 +1,6 @@
 ## Pipenv é uma ferramenta de gerenciamento de pacotes que combina a gestão de dependências com a criação de ambiente virtual para seus projetos e adiciona/remove
 ## pacotes automaticamente do arquivo Pipfile conforme você instala e desinstala pacotes.
 
+pip install pipenv
+# 
+pipenv install django
