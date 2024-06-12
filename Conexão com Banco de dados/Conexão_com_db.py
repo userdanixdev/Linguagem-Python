41cb1c955c5b9a9f2ea45c1354b1061135865b16
userdanixdev <f.daniel.m@gmail.com>
2024-06-11 22:41:50 -0300

    update

--- Conexão com Banco de dados/Conexão_com_db.py
+++ Conexão com Banco de dados/Conexão_com_db.py
@@ -1,8 +1,30 @@
 import sqlite3
 from  pathlib import Path
 
-ROOT_PATH = Path(__file__).parent
+# Criar dentro da pasta 
+ROOT_PATH = Path(__file__).parent 
 
+# Conexão com o banco de dados:
+conection = sqlite3.connect(ROOT_PATH / "clientes.sqlite")
+# declarar uma variável para o executar os comandos da conexão
+cursor = conection.cursor() 
 
-conection = sqlite3.connect(ROOT_PATH / "clientes.db")
-print(conection)
+def criar_tabela(conection,cursor):
+    cursor.execute("CREATE TABLE clientes (ID INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100),email VARCHAR(150))")
+    conection.commit()
+# Inserindo registros: 
+def inserir_registro(conection,cursor,nome,email):
+    data = (nome,email)
+    cursor.execute("INSERT INTO clientes (nome,email) VALUES (?,?);",data)
+    conection.commit()
+def atualizar_registro(conection,cursor,nome,email,ID):
+    data =(nome,email,ID)
+    cursor.execute("UPDATE clientes SET nome=?,email=? WHERE ID=?;",data)    
+    conection.commit()
+def excluir_registro(conection,cursor,id):
+    data=(id,)
+    cursor.execute("DELETE FROM clientes WHERE id=?;",data)
+    conection.commit()
+
+excluir_registro(conection,cursor,1)    
+#atualizar_registro(conection,cursor,"Geovana","geo@gmail.com",2)    
