f7ac2801046ce5bc009f72c3551340bb573c9456
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-15 20:48:32 -0300

    Create Somando_valores_vs6_POO.py

---
+++ Exercicios_curso_em_video/somando_valores/Somando_valores_vs6_POO.py
@@ -0,0 +1,31 @@
+# Somando valores: Versão 6 : POO
+
+# Somando valores: Versão 6 : POO
+
+class SomaValores:
+    def __init__(self):
+        self.n1 = None
+        self.n2 = None
+        self.soma = None
+    def obter_valores(self):
+        while True:
+            n1=input('Insira um valor: ')
+            n2=input('Insira um valor: ')
+            if not (n1.isnumeric() and n2.isnumeric()):
+                print('Somente números inteiros.')
+                continue                  
+            self.n1=int(n1)
+            self.n2=int(n2)
+            break
+    def calcular_soma(self):
+        self.soma = self.n1  + self.n2
+    def exibir_resultado(self):
+        print(f'A soma entre o número {self.n1} + {self.n2} = {self.soma}.')
+    def executar(self):
+        self.obter_valores()
+        self.calcular_soma()
+        self.exibir_resultado()
+
+if __name__ == '__main__':
+    soma_valores=SomaValores()
+    soma_valores.executar()
