6747b4a52e189445c3b65f32e3a7f2c01335d429
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-15 19:15:13 -0300

    Create somando_valores_vs05.py

---
+++ Exercicios_curso_em_video/somando_valores/somando_valores_vs05.py
@@ -0,0 +1,6 @@
+# Somando valores versão 04:
+
+n1=input('Digite um valor: ').isnumeric()  # retorno se for string será falso, se for inteiro: True
+n2=input('Digite outro valor: ').isnumeric() # Retorno se for Str será False, se for INT será True
+soma =  int(n1)+int(n2)  # Podemos colocar o tipo de dado aqui ou dentro do print conforme a versão 03.
+print('A soma entre',n1,'e',n2,'é igual a',soma,'!')
