c025ff63015b2d8283fb3326afbd5d77b16c069e
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-15 18:11:46 -0300

    Update Somando_valores.py

--- Exercicios_curso_em_video/Somando_valores.py
+++ Exercicios_curso_em_video/Somando_valores.py
@@ -6,7 +6,7 @@ s = n1+n2
 print(f' A soma entre {n1} e {n2} é igual a: {s}')
 # Obs: Sem converter o tipo do dado, a variável será 'str', padrão do Python, assim o resultado será concatenado.
 
-# Somando valores versão 02:
+# Somando valores da forma correta:
 
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
