9688af084f4967cd68c0fa8ae8008cba1f16f262
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-15 19:05:48 -0300

    Create somando_valores_vs4.py

---
+++ Exercicios_curso_em_video/somando_valores/somando_valores_vs4.py
@@ -0,0 +1,7 @@
+# Somando valores versão 04:
+
+n1=input('Digite um valor: ')
+n2=input('Digite outro valor: ')
+soma =  int(n1)+int(n2)  # Podemos colocar o tipo de dado aqui ou dentro do print conforme a versão 03.
+print('A soma entre',n1,'e',n2,'é igual a',soma,'!')
+
