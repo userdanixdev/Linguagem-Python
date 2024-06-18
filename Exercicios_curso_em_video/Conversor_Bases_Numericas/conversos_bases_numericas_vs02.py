2c7f6401cef8702f2a09c77e47609244661b443b
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-18 19:14:53 -0300

    Update conversos_bases_numericas_vs02.py

--- Exercicios_curso_em_video/Conversor_Bases_Numericas/conversos_bases_numericas_vs02.py
+++ Exercicios_curso_em_video/Conversor_Bases_Numericas/conversos_bases_numericas_vs02.py
@@ -3,7 +3,7 @@
 # Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
 # 1 para binário, 2 para octal e 3 para hexadecimal.
 
-# Versão 02:
+# Versão 02: Versão em que solicita input do usuário 3 vezes e depois o programa pergunta se quer continuar ou não.
 
 while True:
     for _ in range(3):
