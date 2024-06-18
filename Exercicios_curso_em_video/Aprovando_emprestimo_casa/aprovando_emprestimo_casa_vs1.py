6ff18c083c58d31e08a6e58e4cc941ab2a37856f
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-18 15:30:14 -0300

    Create aprovando_emprestimo_casa_vs1.py

---
+++ Exercicios_curso_em_video/Aprovando_emprestimo_casa/aprovando_emprestimo_casa_vs1.py
@@ -0,0 +1,19 @@
+# Aprovando empréstimo:
+
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+# Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+# A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+
+# Versão 01:
+
+casa = float(input('Valor da casa: R$ '))
+salario = float(input('Salario do comprador: R$'))
+anos = int(input('Quantos anos de financiamento? '))
+prestacao = casa / (anos*12)
+minimo=salario * 30/100
+print(f'Para pagar uma casa de R${casa:.2f} em {anos} anos. ')
+print(f'a prestação será de R${prestacao:.2f}.')
+if prestacao <= minimo:
+    print('Empréstimo pode ser CONCEDIDO.')
+else:
+    print('Empréstimo NEGADO')
