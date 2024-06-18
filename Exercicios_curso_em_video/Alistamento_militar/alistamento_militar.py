7f7607bbdf0174bec4db6a7301636c3fe3e1d2f5
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-18 19:46:29 -0300

    Update alistamento_militar.py

--- Exercicios_curso_em_video/Alistamento_militar/alistamento_militar.py
+++ Exercicios_curso_em_video/Alistamento_militar/alistamento_militar.py
@@ -1,5 +1,11 @@
+# Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade,
+# se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento.
+# Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+# Versão 01:
 
 from datetime import date
+
 atual = date.today().year
 nascimento = int(input('Ano de nascimento: '))
 idade = atual - nascimento
