2def95d39771c3bae47cffa014cdc6544d76ac68
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-18 15:30:44 -0300

    Create aprovando_emprestimo_casa_vs02.py

---
+++ Exercicios_curso_em_video/Aprovando_emprestimo_casa/aprovando_emprestimo_casa_vs02.py
@@ -0,0 +1,20 @@
+# Versão 02:
+
+print()
+print('Aprovador de emprestimo, vs1.1')
+# inserção de dados:
+pay = int(input('QUal o salario? R$'))
+valor_casa = int(input('Qual o valor da casa que quer financiar? R$'))
+tempo=int(input('Em quantos anos vai pagar? '))
+# calculos:
+pay_30=salario*0.3
+total_juros=valor_casa*(1+0.03)**tempo
+prestacao=total_juros/(tempo*12)
+# condições:
+if pay_30>=prestacao:
+    print('financiamento aprovado')
+    print(f'Voce ira pagar R${prestacao:.2f} por mês.')
+    print(f'Total do financiamento: R$ {total_juros:.2f}')
+else:
+    print('Financiamento não aprovado, mensalidade superior a 30% do salario.')
+print('fim')
