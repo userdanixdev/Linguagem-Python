357a673db66e170e4a1523c435846372593a34cf
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-20 20:31:20 -0300

    Create tuplas_tabela_brasileiro_vs04.py

---
+++ Exercicios_curso_em_video/Tuplas_Tabela_Brasileiro/tuplas_tabela_brasileiro_vs04.py
@@ -0,0 +1,32 @@
+# Tabela do Brasileiro 2023 :
+# Versão 04:
+
+
+
+times=('Palmeiras','Atlético-MG','Flamengo','Grêmio','Botafogo',
+       'RB Bragantino','Fluminense','Athletico','São Paulo',
+       'Cuiabá','Corinthians','Cruzeiro','Santos','Vasco','Bahia','Goiás',
+       'Coritiba','América-MG','Atlético-GO','Fortaleza')
+
+print('Campeonato Brasileiro 2023')
+for posicao,time in enumerate(times):
+    print(f'{posicao+1}º{time}.')
+print('\nOs 5 primeiros colocados: ')    
+for posicao,time in enumerate(times[0:5]):
+    print(f'{posicao+1}º{time}.')
+print('\nOs 4 últimos rebaixados: ')    
+for posicao,time in enumerate(times[-4:]):
+    print(f'{posicao+17}º{time}.')
+print('\nClubes em ordem alfabética: ')
+for posicao,time in enumerate(sorted(times)):
+    print(f'{posicao+1}-{time}.')
+clube=input('Digite o nome do clube: ').capitalize()
+if clube in times:
+    print(f'O time {clube} está na {times.index(clube)+1}º posição.')
+    
+
+
+
+        
+              
+        
