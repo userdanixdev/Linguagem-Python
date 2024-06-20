a24b8a5458c5699e9676bea84ac2cdccae754410
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-20 20:44:56 -0300

    Create tupla_tabela_brasileiro_vs05.py

---
+++ Exercicios_curso_em_video/Tuplas_Tabela_Brasileiro/tupla_tabela_brasileiro_vs05.py
@@ -0,0 +1,25 @@
+# Tabela do Brasileiro 2023 :
+# Versão 05: com laço for e range
+
+times=('Palmeiras','Atlético-MG','Flamengo','Grêmio','Botafogo',
+       'RB Bragantino','Fluminense','Athletico','São Paulo',
+       'Cuiabá','Corinthians','Cruzeiro','Santos','Vasco','Bahia','Goiás',
+       'Coritiba','América-MG','Atlético-GO','Fortaleza')
+
+print('Campeonato Brasileiro 2023')
+print('Tabela completa:\n')
+for contagem in range(0,20):
+    print(f'{contagem+1}º{times[contagem]}.')
+print('\nOs 5 primeiros colocados:\n')
+for contagem in range(0,5):
+    print(f'{contagem+1}º{times[contagem]}.')
+print('\nOs 4 últimos colocados:\n')
+for contagem in range(16,20):
+    print(f'{contagem+1}º{times[contagem]}')
+print('\nTabela em ordem alfabética:\n')
+for contagem in range(0,20):
+    print(f'{contagem+1}-{sorted(times)[contagem]}.')
+    
+    
+              
+        
