fdf8914fbc97e88ca758f9583ee68f786bebf991
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-21 20:35:08 -0300

    Update extraindo_vogais_tuplas_vs04.py

--- Exercicios_curso_em_video/Extraindo_vogais_tuplas/extraindo_vogais_tuplas_vs04.py
+++ Exercicios_curso_em_video/Extraindo_vogais_tuplas/extraindo_vogais_tuplas_vs04.py
@@ -1,4 +1,8 @@
-print()
+# Extraindo vogais de uma tupla:
+# Versão 04:
+
+# Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
+# Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
 contagem = 0
 vogais_find = ''
@@ -8,8 +12,8 @@ for c in range(0,len(palavras)):
             contagem += 1
             vogais_find += palavras[c][d]
     print(f'Na palavra: {palavras[c]} tem {contagem} vogais. São elas: {vogais_find}')
-    contagem = 0
-    vogais_find = ''
+    contagem = 0      # Se não vai somar cada vogais e o resultado será diferente.
+    vogais_find = ''  # Se não vai contar as vogais das outras palavras. 
     
             
     
