681b9305327867d3f3db96aabee1146d413903e8
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-21 18:48:28 -0300

    Update listagem_precos_tabular_vs02.py

--- Exercicios_curso_em_video/listagem_precos/listagem_precos_tabular_vs02.py
+++ Exercicios_curso_em_video/listagem_precos/listagem_precos_tabular_vs02.py
@@ -19,3 +19,7 @@ for c in range(0,len(listagem),2):
     print(f'{listagem[c]:.<34}',end='')
     print(f'R$ {listagem[c+1]:>7.2f}')
 print('-'*45)    
+# Fazendo em uma linha:
+for c in range(0,len(listagem),2):
+    print(f'{listagem[c]:.<34}'f'R$ {listagem[c+1]:>7.2f}')
+print('-'*45)    
