75c622059874868582f602f99091d242d72cc74f
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-06-24 02:04:06 -0300

    Update simulador_bancario_vs2.1.py

--- Desafios_code_DIO/Projeto_Simulador_bancario/simulador_bancario_vs2.1.py
+++ Desafios_code_DIO/Projeto_Simulador_bancario/simulador_bancario_vs2.1.py
@@ -3,6 +3,30 @@
 import re
 from datetime import datetime
 usuarios = []
+# Novas opções: Nova opção de usuários
+        
+      
+        # Em python, as funções devem ser definidas antes de serem chamadas.
+def validar_cpf(cpf):
+# remove caracteres não numéricos:
+    cpf = re.sub(r'\D', '',cpf)
+    # Verifica se o CPF tem 11 dígitos:
+    if len(cpf) != 11:
+     return False
+    # Verifica se todos os dígitos são iguais (caso especial de CPF inválido)
+    if cpf == cpf[0]*11:
+     return False
+    # Cálculo do primeiro dígito verificador:
+    soma = sum(int(cpf[i])*(10-i)for i in range(9))
+    primeiro_digito_verificador = 11 - (soma%11)
+    primeiro_digito_verificador = 0 if primeiro_digito_verificador >= 10 else primeiro_digito_verificador
+    # Cálculo do segundo digito verificador:
+    soma = sum(int(cpf[i])*(11-i)for i in range(10))
+    segundo_digito_verificador = 11 - (soma%11)
+    segundo_digito_verificador = 0 if segundo_digito_verificador >= 10 else segundo_digito_verificador
+    # Verifica se os dígitos verificadores estão corretos:
+    return cpf[-2:] ==  f'{primeiro_digito_verificador}{segundo_digito_verificador}'
+        
 menu = '''
         [d] Depositar
         [s] Sacar
@@ -73,50 +97,26 @@ while True:
         import os
         exit()
     elif opcao == 'nu':
-        # Novas opções: Nova opção de usuários
-        import re
-        from datetime import datetime
-        usuarios = []  # Variável vazia recebe lista vazia
-        # Em python, as funções devem ser definidas antes de serem chamadas.
-        def validar_cpf(cpf):
-            # remove caracteres não numéricos:
-             cpf = re.sub(r'\D', '',cpf)
-             # Verifica se o CPF tem 11 dígitos:
-             if len(cpf) != 11:
-              return False
-            # Verifica se todos os dígitos são iguais (caso especial de CPF inválido)
-             if cpf == cpf[0]*11:
-                return False
-        # Cálculo do primeiro dígito verificador:
-             soma = sum(int(cpf[i])*(10-i)for i in range(9))
-             primeiro_digito_verificador = 11 - (soma%11)
-             primeiro_digito_verificador = 0 if primeiro_digito_verificador >= 10 else primeiro_digito_verificador
-        # Cálculo do segundo digito verificador:
-             soma = sum(int(cpf[i])*(11-i)for i in range(10))
-             segundo_digito_verificador = 11 - (soma%11)
-             segundo_digito_verificador = 0 if segundo_digito_verificador >= 10 else segundo_digito_verificador
-        # Verifica se os dígitos verificadores estão corretos:
-             return cpf[-2:] ==  f'{primeiro_digito_verificador}{segundo_digito_verificador}'
-        
-        while True:  # Loopíng infinito
+             
+     while True:  # Loopíng infinito
          print('1.Criar usuário.')
          print('2.Sair')
-        opcao = input('Escolha uma opção: ')
-        if opcao == '1':
+         opcao = input('Escolha uma opção: ')
+         if opcao == '1':
             while True:
                 cpf = input('Informe o CPF: ')
-            if validar_cpf(cpf):
-                print('CPF válido.')
-                break
-            else:
-                print('CPF inválido. Tente novamente.')
-        usuario_existe = False
-        for usuario in usuarios:
+                if validar_cpf(cpf):
+                 print('CPF válido.')
+                 break
+                else:
+                    print('CPF inválido. Tente novamente.')
+         usuario_existe = False
+         for usuario in usuarios:
             if usuario['cpf'] == cpf:
                 print('\nJá existe usuário com esse CPF')
                 usuario_existe = True
                 break
-        if not usuario_existe:
+         if not usuario_existe:
             while True:
                     nome_usuario = input('Informe o nome completo: ')
                     if nome_usuario.replace(' ','').isalpha():  # 'isalpha verifica se contem apenas letras
@@ -168,11 +168,11 @@ while True:
             endereco = {'cidade':cidade,'estado':estado,'bairro':bairro,'rua':rua,'numero_casa':numero_casa}                    
             usuarios.append({'nome':nome,'data_nascimento':data_nascimento,'cpf':cpf,'endereco':endereco})
             print('Usuario criado com sucesso')
-    elif opcao == '2':
-        print('encerrando o programa')
-        break
-    else:
-        print('Opcao inválida. Tente novamente.')
+         elif opcao == '2':
+            print('encerrando o programa')
+            break
+         else:
+            print('Opcao inválida. Tente novamente.')
 print('\nLista de Usuários: ')
 for usuario in usuarios:
     print(f'\nNome: {usuario['nome']},\nCPF:{usuario['cpf']},\nData de Nascimento:{usuario['data_nascimento']},\nEndereço:{usuario['endereco']}\n'
