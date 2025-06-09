2c0b337528cf70b4498c165004ba29c6971d4b97
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2025-06-09 18:39:14 -0300

    Update sintaxe.py

--- Sintaxe/sintaxe.py
+++ Sintaxe/sintaxe.py
@@ -1,3 +1,17 @@
 Còdigos do projeto Sintaxe da Lingua Portuguesa.
 
 
+Estudos da sintaxe:
+
+                [1] - Introdução aos termos da oração: Sujeitos de uma oração
+                [2] - Exercicícios de fixação: SUJEITOS de uma oração
+                [3] - Predicação Verbal e Nominal + Exercícios
+                [4] - Transitividade Verbal
+                [5] - Regência Verbal
+                [6] - Complemento Nominal X Adjunto Adnominal
+                [0] - Sair
+
+
+
+  é um protótipo.
+
