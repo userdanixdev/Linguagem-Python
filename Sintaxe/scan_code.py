4387a4a5ec7fbae67e325b043f7951511a6cdd5e
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2025-08-01 02:12:19 -0300

    Update scan_code.py

--- Sintaxe/scan_code.py
+++ Sintaxe/scan_code.py
@@ -116,6 +116,10 @@ Isso é ideal se você quer estabilidade entre layouts (como ABNT e US), mas cui
 o usuário ainda verá letras/símbolos diferentes dependendo do layout ativo.
 
 
+Se seu objetivo é garantir que qualquer usuário, com qualquer layout de teclado, consiga usar seu programa sem erros,
+usar scan_code é a melhor escolha — ele ignora o layout lógico do teclado e usa a posição física da tecla.
+
+
 
 
 
