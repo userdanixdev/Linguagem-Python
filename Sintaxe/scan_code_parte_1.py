9696c67de74846dac397f122eeb53ed05b628839
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2025-08-01 02:18:14 -0300

    Create scan_code_parte_1.py

---
+++ Sintaxe/scan_code_parte_1.py
@@ -0,0 +1,2 @@
+Se seu objetivo é garantir que qualquer usuário, com qualquer layout de teclado, consiga usar seu programa sem erros,
+usar scan_code é a melhor escolha — ele ignora o layout lógico do teclado e usa a posição física da tecla.
