d6519b96ca5ddd6247ce060fcf547bb2501cc32d
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2025-12-02 16:13:40 -0300

    Update _init_&_del_.py

--- Prog_POO/_init_&_del_.py
+++ Prog_POO/_init_&_del_.py
@@ -1,15 +1,21 @@
-#esafio
-#	Crie um programa onde joão informe: cor, modelo, ano e valor da bicicleta vendida. Uma bicicleta pode: buzinar, parar e correr.
+# Desafio:
+#	Crie um programa onde o usuário informa: nome, raça, idade do cachorro.
+	# Um cachorro pode: latir, correr e sentar
 #   Adicione esses comportamentos.
 
+# O Objetivo desse estudo é além de mostrar o comportamento do objeto. Como também mostrar os métodos especiais '__init__'
+# mostrando uma mensagem inicializando. E o método '__del__' que deleta os comportamentos do objeto, no final do programa.
+
 class cachorro:
-    def __init__(self,nome,raca,idade, acordado=True):		
+    def __init__(self,nome,raca,idade, acordado=True):		# <- Método construtor com os atributos 'self' e os argumentos e '__init__'
+     print('Inicializando dados do cachorro...') #<- Mensagem de inicialização    
      self.nome = nome           #<- Atributos da classe
      self.raca = raca     		#<- Atributos da classe
      self.idade = idade			#<- Atributos da classe
      self.acordado = acordado   #<- Atributos da classe
      
-
+    def __del__(self):				#<- Método destrutor, executado no final do programa
+     print(f'O cachorro {self.nome} foi deletado da memória.')
 # Agora preciso colocar 3 comportamentos para a bicicleta. Os métodos são bem parecidos com funções que estão dentro de uma classe.
     def latir (self):
         print('AU AU AU!!')
@@ -18,28 +24,52 @@ class cachorro:
     def correr (self):
         print('Vapt Vupt!!')
 
-def __str__(self): #<- Método especial para representar o objeto como strings
-	return f"{self.__class__.__name__}: {[f'{chave}={valor}'for chave, valor in self.__dict__.items()]}"
+    def __str__(self): #<- Método especial para representar o objeto como strings
+        return f"{self.__class__.__name__}: {[f'{chave}={valor}'for chave, valor in self.__dict__.items()]}"
 
-def dog_novo():        
+def dog_novo():        # Método para criar um novo cachorro. Inicializa o programa de novo, sem cadastro. E no final deleta.
     d = cachorro("Rex","Pastor Alemão",5,True)
     print(d)
 
 # Os comportamentos da classe do cachorro são definidos por métodos. Para definir o método usamos em Python a palavra reservada 'def'
 # e pelo menos dentro do comportamento, entre parênteses, um argumento, obrigatoriamente, chamado 'self'
 # Usuário informa os dados do cachorro:
-nome = input("Digite a cor da bicicleta: ")
-raca = input("Digite o modelo da bicicleta: ")
-idade = int(input("Digite o ano da bicicleta: "))
+nome = input("Digite o nome do dog: ")
+raca = input("Digite a raça do dog: ")
+idade = int(input("Digite a idade do dog: "))
 
-c = cachorro(nome, raca, idade, acordado) #<- Instância da classe + argumentos
+c = cachorro(nome, raca, idade, acordado=False) #<- Instância da classe + argumentos
 
 print("\n--- Cachorro cadastrado ---")
-print(f"Nome: {c.cor}")
-print(f"Raça: {c.modelo}")
-print(f"Idade: {c.ano}")
+print(f"Nome: {c.nome}")
+print(f"Raça: {c.raca}")
+print(f"Idade: {c.idade} anos")
 
 print("\n--- Testando comportamentos ---")
 c.latir()
 c.sentar()
 c.correr()
+print(c)
+dog_novo()
+
+Resultadados mostrados na tela:
+
+Digite o nome do dog: Star
+Digite a raça do dog: Pitbull
+Digite a idade do dog: 5
+Inicializando dados do cachorro...
+
+--- Cachorro cadastrado ---
+Nome: Star
+Raça: Pitbull
+Idade: 5 anos
+
+--- Testando comportamentos ---
+AU AU AU!!
+Cachorro sentado.
+Vapt Vupt!!
+cachorro: ['nome=Star', 'raca=Pitbull', 'idade=5', 'acordado=False']
+Inicializando dados do cachorro...  # Aqui ele inicializa novamente, porque foi criado um novo método. Sem cadastro. E mostra os dados.
+cachorro: ['nome=Rex', 'raca=Pastor Alemão', 'idade=5', 'acordado=True']
+O cachorro Rex foi deletado da memória.
+O cachorro Star foi deletado da memória.
