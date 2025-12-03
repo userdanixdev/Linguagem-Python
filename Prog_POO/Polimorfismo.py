67cbe641dce0032ac95632f1f8a5c2d51ebf9466
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2025-12-02 22:01:18 -0300

    Update Polimorfismo.py

--- Prog_POO/Polimorfismo.py
+++ Prog_POO/Polimorfismo.py
@@ -1,1 +1,8 @@
 # Polimorfismo:
+
+#Como o polimorfismo se encaixa com herança. A palavra polimorfismo significa ter muitas formas. 
+#Na programação, polimorfirsmo significa o mesmo nome de função (mas assinaturas diferentes) sendo usado para tipos diferentes.
+#No entanto, o polimorfismo com herança, o mesmo método, herdado da classe pai, é possível modificar a classe herança mudando seu
+#comportamento. Isso é útil em determinados casos em que a classe filha não se encaixa perfeitamente nos métodos da classe pai.
+
+
