beb41b390881fc3800d67d3b12236f4694fc1e69
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2025-12-06 23:56:33 -0300

    Update 2_conection_ORM.py

--- Formação Python Developer - DIO/SQL Alchemy/2_conection_ORM.py
+++ Formação Python Developer - DIO/SQL Alchemy/2_conection_ORM.py
@@ -9,5 +9,44 @@ import sqlalchemy
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import Column, Integer, String, ForeiggnKey
 
+data_base = declarative_base()
+
+class User(data_base):
+    __tablename__ = "user_account" # Nome da tabela
+    # Definir coluna, atributos e restrições:
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    ft_name = Column(String)
+    fl_name = Column(String)
+    # O correto é nome da propriedade, não o nome da classe na relação.
+    addresses=relationship("Address", back_populates= "user", cascade="all, delete-orphan")
+    # OPCIONAL: construtor para usar argumentos posicionais
+    def __init__(self,id, ft_name, fl_name):
+        self.id = id
+        self.ft_name = ft_name
+        self.fl_name = fl_name
+    def __repr__(self):  # Método especial 'repr' representando a classe USER, ENTIDADE.
+        return f"User (id={self.id}, first_name={self.ft_name}, full_name:{self.fl_name})"
+
+class Address(data_base):
+    __tablename__ = "Address"
+    id = Column(Integer, primary_key=True, autoincrement = True)
+    email_address = Column (String(40), nullable=False) # Não poderá ter campos nulos
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
+    user = relationship("User", back_populates="addresses" )
+    # OPCIONAL: construtor para usar argumentos posicionais
+    def __init__(self,user_id, email_address):
+        self.user_id = user_id
+        self.email_address = email_address    
+    def __repr__(self): # Método especial 'repr' representando a classe Address, ENTIDADE.
+        return f"Address (id={self.id}, e-mail={self.email_address})"
+
+
+# SQL Alchemy permite parâmetros posicionais, sendo desnecessários o uso abaixo para testes:
+#candidato_1 = User(id=1,ft_name="Daniel",fl_name="Martins")
+#print(candidato_1)        
+# SQL Alchemy permite parâmetros posicionais
+candidato_2 = User(2,"Daniel","Martins")
+print(candidato_2)
+
 
 
