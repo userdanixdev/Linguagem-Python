9494dd4e647da34a876dd204fc0487d2b74e16e5
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2025-12-09 16:29:04 -0300

    Update fase_2.py

--- Formação Python Developer - DIO/SQL Alchemy/Desafio/fase_2.py
+++ Formação Python Developer - DIO/SQL Alchemy/Desafio/fase_2.py
@@ -1,1 +1,10 @@
 # Fase 02:
+# Regras de negócio dentro das entidades:
+  # métodos sobrescritos pode_sacar
+  # lógica de limite especial lógica de rendimento mensal, cálculo de resgate com taxa e atomicidade com Session.
+
+# Implementação dos serviços:
+  # Serviço de transações
+  # Serviço de relatórios (consultas avançadas)
+      # Consultas avançadas do banco como: total movimentado por cliente, ranking de clientes por movimentação
+        # e histórico filtrado por período e saldo consolidado.
